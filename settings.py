dc3185151a83dbc3f87f2650abc5c2e4222d88db
Genkinston <genkinston@vk.com>
2026-03-06 15:20:53 +0700

    feat(gameplay): Add ship movement functionality
    The ship can now move left and right based on keyboard inputs.
    - Added `self.ship.update()` in the main game loop to update the ship's position.
    - Modified `_check_events` method to handle `KEYDOWN` and `KEYUP` events for the right and left arrow keys, updating `self.ship.moving_right` and `self.ship.moving_left` flags accordingly.
    - Added `self.settings` to the `Ship` class to access the ship's speed setting.
    - Added `self.x` to store the ship's exact position for smoother movement.
    - Added `moving_right` and `moving_left` flags to the `Ship` class to track the ship's movement direction.
    - Implemented `update` method in the `Ship` class to adjust the ship's position based on the movement flags and screen boundaries.
    - Updated `self.rect.x` in the `update` method to reflect the new position stored in `self.x`.

--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (123, 104, 238)
+        # Настройки корабля
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         '''Инициализирует корабль и его начальную позицию.'''
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         #  Загружает изображение корабля и получает его прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,24 @@ class Ship:
 
         # Каждый новый корабль появляется у нижнего края экрана.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
+
+        # Флаг перемещения: начинаем с неподвижного корабля.
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''Обновляет позицию корабля с учетом флага.'''
+        # Обновлет атрибут x, не rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # обновление атрибута rect на основании self.x
+        self.rect.x = self.x
 
     def blitme(self):
         '''Рисует корабль в текущей позиции.'''
