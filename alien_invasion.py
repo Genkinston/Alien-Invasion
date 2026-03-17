17122426c5cf6a02384ae25f3eb39432a21bc139
Genkinston <genkinston@vk.com>
2026-03-17 13:05:10 +0700

    set mouse visible after fail game

--- alien_invasion.py
+++ alien_invasion.py
@@ -152,6 +152,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         """Создает флот пришельцев."""
